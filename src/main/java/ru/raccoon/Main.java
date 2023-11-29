5d93687351d9a55b040482c2ae667797fe98d7fe
sserh <serhios@bk.ru>
2023-11-29 09:05:59 +0700

    Учтены замечания

--- src/main/java/ru/raccoon/Main.java
+++ src/main/java/ru/raccoon/Main.java
@@ -19,40 +19,64 @@ public class Main {
 
         //запускаем потоки с подсчётами
 
-        Thread thread_length3 = new Thread(() -> {
+        Thread threadCase1 = new Thread(() -> {
             for (String str :
                     texts) {
-                if (str.length() == 3 && isBeautyWord(str)) {
-                    counter_length3.getAndAdd(1);
+                if (isCase_1(str)) {
+                    if (str.length() == 3) {
+                        counter_length3.getAndAdd(1);
+                    }
+                    if (str.length() == 4) {
+                        counter_length4.getAndAdd(1);
+                    }
+                    if (str.length() == 5) {
+                        counter_length5.getAndAdd(1);
+                    }
                 }
             }
         });
-        thread_length3.start();
+        threadCase1.start();
 
-        Thread thread_length4 = new Thread(() -> {
+        Thread threadCase2 = new Thread(() -> {
             for (String str :
                     texts) {
-                if (str.length() == 4 && isBeautyWord(str)) {
-                    counter_length4.getAndAdd(1);
+                if (isCase_2(str)) {
+                    if (str.length() == 3) {
+                        counter_length3.getAndAdd(1);
+                    }
+                    if (str.length() == 4) {
+                        counter_length4.getAndAdd(1);
+                    }
+                    if (str.length() == 5) {
+                        counter_length5.getAndAdd(1);
+                    }
                 }
             }
         });
-        thread_length4.start();
+        threadCase2.start();
 
-        Thread thread_length5 = new Thread(() -> {
+        Thread threadCase3 = new Thread(() -> {
             for (String str :
                     texts) {
-                if (str.length() == 5 && isBeautyWord(str)) {
-                    counter_length5.getAndAdd(1);
+                if (isCase_3(str)) {
+                    if (str.length() == 3) {
+                        counter_length3.getAndAdd(1);
+                    }
+                    if (str.length() == 4) {
+                        counter_length4.getAndAdd(1);
+                    }
+                    if (str.length() == 5) {
+                        counter_length5.getAndAdd(1);
+                    }
                 }
             }
         });
-        thread_length5.start();
+        threadCase3.start();
 
         //подождём, пока посчитаются "красивые" слова всех длин
-        thread_length3.join();
-        thread_length4.join();
-        thread_length5.join();
+        threadCase1.join();
+        threadCase2.join();
+        threadCase3.join();
 
         //результат
         System.out.println("Красивых слов с длиной 3: " + counter_length3 + " шт.");
@@ -71,9 +95,9 @@ public class Main {
     }
 
     //метод проверки слова на "красивость"
-    public static boolean isBeautyWord(String word) {
+    public static boolean isCase_1(String word) {
 
-        //проверяем на вхождение в слово только одной буквы (проверку выполняем первой, так как это ещё и палиндром)
+        //проверяем на вхождение в слово только одной буквы
         int counter = 1;
         for (int i = 1; i < word.length(); i++) {
             //если символы равны
@@ -87,17 +111,24 @@ public class Main {
                 }
             }
         }
+        return false;
+    }
 
-        //проверяем, палиндром ли
-        StringBuilder originalWord = new StringBuilder(word);
-        String invertedWord = originalWord.reverse().toString();
-        //если палиндром, то слово "красивое"
-        if (word.equals(invertedWord)) {
-            return true;
+        public static boolean isCase_2(String word) {
+
+            //проверяем - не палиндром ли
+            StringBuilder originalWord = new StringBuilder(word);
+            String invertedWord = originalWord.reverse().toString();
+            //если палиндром, то слово "красивое"
+            if (word.equals(invertedWord)) {
+                return true;
+            }
+            return false;
         }
 
-        //проверяем на "по-алфавитность"
-        counter = 1;
+    public static boolean isCase_3(String word) {
+        //проверяем на по-алфавитность
+        int counter = 1;
         for (int i = 0; i < word.length() - 1; i++) {
             //если последующий символ больше или равен предыдущему
             if (word.charAt(i) <= word.charAt(i + 1)) {
@@ -110,7 +141,6 @@ public class Main {
                 }
             }
         }
-        //если нигде раньше не вернули результат, то слово "некрасивое"
         return false;
     }
 }
